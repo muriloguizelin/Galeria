4826ef7886411d12bf988ce80a84a45fc23c6948
Murilo Guizelin <murilomotta4@gmail.com>
2024-04-01 14:30:37 -0400

    feat

--- imageliteapi/src/main/java/io/github/muriloguizelin/galeria/application/images/ImageDTO.java
+++ imageliteapi/src/main/java/io/github/muriloguizelin/galeria/application/images/ImageDTO.java
@@ -8,9 +8,14 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class ImageDTO {
+
     private String url;
+
     private String name;
+
     private String extension;
+
     private Long size;
+    
     private LocalDate uploadDate;
 }
